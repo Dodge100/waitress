ec12161403a258e189cbc277c554c97f14779798
Dodge1 <varung2235@gmail.com>
2025-05-12 11:27:41 -0700

    initial commit

---
+++ waitress/waitressApp.swift
@@ -0,0 +1,17 @@
+//
+//  waitressApp.swift
+//  waitress
+//
+//  Created by Varun Gundamaraju on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct waitressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waitressTests/waitressTests.swift
@@ -0,0 +1,17 @@
+//
+//  waitressTests.swift
+//  waitressTests
+//
+//  Created by Varun Gundamaraju on 5/12/25.
+//
+
+import Testing
+@testable import waitress
+
+struct waitressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
