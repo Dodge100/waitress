7466be8e798603efc7fe128f4617b66b839bbc42
Dodge1 <varung2235@gmail.com>
2025-05-21 11:29:34 -0700

    restructure

---
+++ waitress/Main/WaitressApp.swift
@@ -0,0 +1,15 @@
+//
+//  IceApp.swift
+//  waitress
+//
+
+import SwiftUI
+
+@main
+struct WaitressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WaitressArea()
+        }
+    }
+}
